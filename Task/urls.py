2e9c7de9bf47edc122643c8ff3ccab03f5d05773
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2022-11-04 19:05:22 +0100

    Update urls.py
    R

--- Task/urls.py
+++ Task/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 urlpatterns=[ 
     path('', views.View.as_view()), 
-    path('operation/', views.OperationView.as_view()
+    path('operation/', views.OperationView.as_view()) 
 ]
