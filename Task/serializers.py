6f1ece9be509862b12afee57a4bb1b7a83eb0259
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2022-11-04 20:04:54 +0100

    Update serializers.py

--- Task/serializers.py
+++ Task/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from decouple import config
 Operation_types=[
 ('addition','addition'),
 ('subtraction','subtraction'), 
@@ -11,4 +11,26 @@ class OperationSerializer(serializers.Serializer):
    x=serializers.IntegerField(required=False)
    y=serializers.IntegerField(required=False)
 
-   
+   def validate_operation_type(self, value):
+     X =['addition', 'subtraction', 'multiplication']
+     if value not in X:
+      
+      import openai
+
+      openai.api_key = config("OPENAI_API_KEY")
+
+ 
+
+      response = openai.Completion.create(
+      model="text-davinci-002",
+      prompt=f"Q: {value} ",
+      temperature=0,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0,
+      stop=["\n"]
+) 
+      print(response) 
+      return {'response':response, 'op':value} 
+     return value 
