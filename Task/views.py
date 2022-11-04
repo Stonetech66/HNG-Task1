93bfa5e1d6d2da37171ccbf0dd18c191be7ebed1
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2022-11-04 16:30:19 +0100

    Update views.py

--- Task/views.py
+++ Task/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import OperationSerializer 
 
 class View(APIView):
     def get(self, *args, **kwargs):
@@ -8,3 +9,25 @@ class View(APIView):
         'age':18,
         'bio':'backend developer with big dreams'}
         return Response(data)
+
+
+class OperationView(APIView):
+   serializer_class= OperationSerializer
+   
+   def post(self):
+      serializer= OperationSerializer
+      if serializers.is_valid():
+         x= serializers.validated_data['x']
+         y= serializers.validated_data['y']
+         op= serializers.validated_data['operation_type']
+         if op == 'addition':
+            result= x + y
+            op_type= 'addition' 
+
+         elif op == 'subtraction':
+            result= x - y
+            op_type= 'addition' 
+         elif op == 'multiplication':
+            result= x * y
+            op_type= 'multiplication'
+         return Response({'slackUsername':'Livingstone', 'result':result, 'operation_type': op_type}) 
